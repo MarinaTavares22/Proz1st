7e850ec16202005602a4c021f6c7f4e532ba8bba
aluno <aluno@embarquedigital.portodigital.local>
2025-06-14 12:16:19 -0300

    subindo aula 07

--- script.js
+++ script.js
@@ -10,8 +10,35 @@ function verificarNumero() {
     if (numero > 0 ) {mensagem = "O Número é positivo.";
     }
     else if ( numero < 0) {
-        mensagem = "O número é Negativo."; }
+        mensagem = "O número é Negativo."; 
+    }
+    else {
+        mensagem= "O número é zero.";
+    }
         
   Document.getElementById("Resultado").innertext = mensagem;   
 }
+
+
+
+function executarFor() {
+    let Resultado = "números pares de 0 a 10: ";
+    for (let i = 0; i <=10; i++) {
+        if(i % 2 === 0)
+            Resultado += i ;
+    }  
+    
+    document. getElementById("saída").textContent = Resultado;
+}
+
+function executarArray() {
+    let nomes = ["Manu", "Samira", "Andreia", "Ana"];
+    let Resultado = "Lista de alunas:" ;
+
+    for (let i = 0; nomes.length; i++) {
+        Resultado += nomes [i]
+ }
+ document.getElementById("saída").textContent = Resultado;
+}
+
 
